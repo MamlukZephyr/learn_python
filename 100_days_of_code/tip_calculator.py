4335c75c95171c817423916d73c6338b14ee7b29
mfarhanz <paan.zainudin97@gmail.com>
2023-07-18 23:29:31 +0800

    edit tip_calculator.py

--- 100_days_of_code/tip_calculator.py
+++ 100_days_of_code/tip_calculator.py
@@ -7,7 +7,7 @@ tip_in_percentage = tip/100
 total_bill = bill * tip_in_percentage
 total_bill_and_tips = bill + total_bill
 total_after_tip = total_bill_and_tips / people
-final_amount = round(total_after_tip, 2)
+# final_amount = round(total_after_tip, 2)
 final_amount = "{:.2f}".format(total_after_tip)
 
 print(f"Each person should pay: ${final_amount}")
